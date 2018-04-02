cd751155c9b736d4878ed5c8047e40367b81a80c
pengcz <pengcz@yiche.com>
2018-04-03 05:46:02 +0800

    first version

---
+++ README.md
@@ -0,0 +1,19 @@
+this project's code is from [
+Spring 4 MVC + JSONP Example with REST, @ResponseBody and ResponseEntity](https://www.concretepage.com/spring-4/spring-4-mvc-jsonp-example-with-rest-responsebody-responseentity),
+the original project used gradle to compile and run in war, for simplicity, pom.xml and   SampleAnnotationApplication.java are added , 
+
+the steps:
+
+1. mvn package; mvn spring-boot:run
+
+2. open file:///jsonptest.html in browser, such as google chrome, under console, there is:
+Godan Premchand
+-----------
+Ramcharitmanas
+TulasiDas
+-----------
+Ramayan
+Valmiki
+----------- 
+
+
---
+++ src/main/java/com/concretepage/SampleAnnotationApplication.java
@@ -0,0 +1,31 @@
+package com.concretepage;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by abelard on 2/5/18.
+ */
+@SpringBootApplication
+public class SampleAnnotationApplication implements CommandLineRunner{
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        //SpringApplication.run(SampleAnnotationApplication.class, args);
+        SpringApplication.run(SampleAnnotationApplication.class, args);
+    }
+/*
+    final private CityMapper cityMapper;
+
+    public SampleAnnotationApplication(CityMapper cityMapper) {
+        this.cityMapper = cityMapper;
+    }
+
+    */
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("");//this.cityMapper.getStyleSummary().get(0).getYmd().toString());
+    }
+}
